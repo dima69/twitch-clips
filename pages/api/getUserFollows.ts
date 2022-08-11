f11c4672fac94eee14050764feb6eacf6f94d745
dimadima <23dima6@gmail.com>
2022-08-11 22:08:33 +0300

    Update getUserFollows.ts
    yeah

--- pages/api/getUserFollows.ts
+++ pages/api/getUserFollows.ts
@@ -7,17 +7,37 @@ export default async function handler(
   res: NextApiResponse
 ) {
   let { user_id } = req.query;
-  const url = `https://api.twitch.tv/helix/users/follows?from_id=${user_id}`;
   try {
-    const response = await axios.get(url, {
+    const response = await axios.get("https://api.twitch.tv/helix/users/follows", {
       headers: {
         Authorization: `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
         "Client-ID": `${process.env.CLIENT_ID}`,
       },
+      params: {
+        from_id: user_id
+      }
     });
-    res.status(200).json(response.data);
+
+    let array_of_ids = [];
+    let { data } = response.data;
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        const element = data[key];
+        array_of_ids.push(element.to_id);
+      }
+    };
+    const response2 = await axios.get("https://api.twitch.tv/helix/users", {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+        "Client-ID": `${process.env.CLIENT_ID}`,
+      },
+      params: {
+        id: array_of_ids,
+      },
+    });
+    res.status(200).json(response2.data.data);
   } catch (error) {
-    console.log("something is wrong... falling back to mock data", error);
+    console.log("something is wrong... falling back to mock data");
     res.status(400).json(userFollowsMockData);
   }
 }

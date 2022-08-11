1c2cfb9150e28841927bb70fbba0bf5f64ff2d99
dimadima <23dima6@gmail.com>
2022-08-11 21:19:37 +0300

    new api routes

---
+++ pages/api/getClips.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import clipsMockData from "./clipsMockData.json";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let { user_id } = req.query;
+  const url = `https://api.twitch.tv/helix/clips?broadcaster_id=${user_id}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+        "Client-ID": `${process.env.CLIENT_ID}`,
+      },
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log("something is wrong... falling back to mock data", error);
+    res.status(400).json(clipsMockData);
+  }
+}

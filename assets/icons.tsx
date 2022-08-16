f9532164e8f0176d7ad6a984d449995b2c15ef15
dimadima <23dima6@gmail.com>
2022-08-16 15:52:24 +0300

    icons
    rename close and people icons, add loading spinner icon

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -1,4 +1,4 @@
-export const PeopleIcon = () => (
+export const CloseIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-6 w-6"
@@ -15,7 +15,7 @@ export const PeopleIcon = () => (
   </svg>
 );
 
-export const CloseIcon = () => (
+export const PeopleIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="h-6 w-6"
@@ -46,5 +46,28 @@ export const SearchIcon = () => (
       strokeLinejoin="round"
       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
     />
+  </svg>
+);
+
+export const LoadingSpinnerIcon = () => (
+  <svg
+    className="animate-spin"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-60"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
   </svg>
 );

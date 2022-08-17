1460a6a78e322a38f1c5d6b31097dc0b0031363c
dimadima <23dima6@gmail.com>
2022-08-17 17:05:49 +0300

    add interface type for clip

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,25 @@ const getUserFollows = async (user_id: string) => {
   );
 };
 
+interface IClip {
+  id: string;
+  url: string;
+  embed_url: string;
+  broadcaster_id: string;
+  broadcaster_name: string;
+  creator_id: string;
+  creator_name: string;
+  video_id: string;
+  game_id: string;
+  language: string;
+  title: string;
+  view_count: number;
+  created_at: string;
+  thumbnail_url: string;
+  duration: number;
+  vod_offset: null;
+}
+
 const UserClips = ({ user_id }: { user_id: string }) => {
   const { data, error } = useSWR(user_id, getClips);
 

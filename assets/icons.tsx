bcc1992490b3a9f513b81ee1a23cc88ced4995d4
dimadima <23dima6@gmail.com>
2022-08-25 15:04:27 +0300

    add chevron down icon

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -68,3 +68,20 @@ export const LoadingSpinnerIcon = () => (
     ></path>
   </svg>
 );
+
+export const ChevronDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-4 h-4 ml-2"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+    />
+  </svg>
+);

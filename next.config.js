a8f000a7aebac88ff1eba0bfbd5f216d91d8c56e
dimadima <23dima6@gmail.com>
2022-09-09 00:02:37 +0300

    upgrade packages, remove experimental flag
    no need

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     domains: ["clips-media-assets2.twitch.tv", "static-cdn.jtvnw.net"],
   },
-  experimental: { images: { allowFutureImage: true } }
 };
 
 module.exports = nextConfig;
